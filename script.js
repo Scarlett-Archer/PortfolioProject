faf381e2ff5d0d1081c2ccde1a3fea89c82f83f0
Scarlett Archer <scarlett_archer6@yahoo.com>
2023-11-26 11:42:01 +0000

    add the variables containing the messages

---
+++ script.js
@@ -0,0 +1,19 @@
+const randomActsOfKindness = [
+  "Call a friend that you haven’t spoken to for a while",
+  "Send flowers to a friend",
+  "Tell your family members how much you love and appreciate them",
+  "Help your parents with household chores",
+  "Tell someone you know that you are proud of them",
+  "Tell your family members how much you love and appreciate them",
+  "Have a clear out and take items to a charity shop",
+  "Compliment a stranger or someone you know",
+];
+
+const positivePhrase = [
+  "Make someone's day! It'll make you smile.",
+  "Have a great day!",
+  "You've got this!",
+  "You're doing great!",
+];
+
+
