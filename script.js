18d0698256a796f9a1740aca523746493a1e29b8
Scarlett Archer <scarlett_archer6@yahoo.com>
2023-11-26 12:43:23 +0000

    Add function and an additional variable containing array.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const randomActsOfKindness = [
-  "Call a friend that you haven’t spoken to for a while",
-  "Send flowers to a friend",
-  "Tell your family members how much you love and appreciate them",
-  "Help your parents with household chores",
-  "Tell someone you know that you are proud of them",
-  "Tell your family members how much you love and appreciate them",
-  "Have a clear out and take items to a charity shop",
-  "Compliment a stranger or someone you know",
+  "Call a friend that you haven’t spoken to for a while...",
+  "Send flowers to a friend...",
+  "Tell your family members how much you love and appreciate them...",
+  "Help your parents with household chores...",
+  "Tell someone you know that you are proud of them...",
+  "Tell your family members how much you love and appreciate them...",
+  "Have a clear out and take your items to a charity shop...",
+  "Compliment a stranger or someone you know...",
+];
+
+const motivationalMessage = [
+  "When you give joy to other people, you get more joy in return.",
+  "Learn as if you will live forever, live like you will die tomorrow.",
+  "Live life to the fullest, and focus on the positive.",
+  "You'll never do a whole lot unless you're brave enough to try",
+  "Always turn a negative situation into a positive situation.",
+  "Great things never come from comfort zones.",
 ];
 
 const positivePhrase = [
@@ -16,4 +25,13 @@ const positivePhrase = [
   "You're doing great!",
 ];
 
+const getRandomPhrase = (array) => {
+  let randomInd = Math.floor(Math.random() * array.length);
+  return array[randomInd];
+};
 
+console.log(
+  `Today, your random act of kindness is: ${getRandomPhrase(
+    randomActsOfKindness
+  )}`
+);
